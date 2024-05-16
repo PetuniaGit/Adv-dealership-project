e647d70be1fb9d9b5934b803693fa797fa9e4af5
Petunia Woldegerima <petuniatwg@gmail.com>
2024-05-16 11:28:44 -0400

    second class

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,4 +1,40 @@
 package com.pluralsight;
 
 public class LeaseContract extends Contract{
+    private double expectedEndingValue;
+    private double leaseFee;
+    private double monthlyPayment;
+
+    public LeaseContract(String contractDate, String customerName, String customerEmail, Vehicle vehicleSold, double expectedEndingValue, double leaseFee) {
+        super(contractDate, customerName, customerEmail, vehicleSold);
+        this.expectedEndingValue = expectedEndingValue;
+        this.leaseFee = leaseFee;
+    }
+
+    public double getExpectedEndingValue() {
+        return expectedEndingValue;
+    }
+
+    public void setExpectedEndingValue(double expectedEndingValue) {
+        this.expectedEndingValue = expectedEndingValue;
+    }
+
+    public double getLeaseFee() {
+        return leaseFee;
+    }
+
+    public void setLeaseFee(double leaseFee) {
+        this.leaseFee = leaseFee;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return 0.00802*getVehicleSold().getPrice()*36 ;
+    }
+
+    @Override
+    public double getMonthlypayment() {
+        return 0.00802*getVehicleSold().getPrice();
+    }
 }
+git

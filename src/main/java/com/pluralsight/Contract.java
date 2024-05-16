3017a4fb796c318009ef88e1fd1b7d5866f5e0f7
Petunia Woldegerima <petuniatwg@gmail.com>
2024-05-16 10:52:09 -0400

    sales contract added

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,7 +1,51 @@
 package com.pluralsight;
 
-public class Contract {
-    public static void main(String[] args) {
-        System.out.println("hello");
+public abstract class Contract {
+    private String contractDate;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+    private double TotalPrice;
+    private double Monthlypayment;
+
+    public Contract(String contractDate, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.contractDate = contractDate;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
     }
+
+    public String getContractDate() {
+        return contractDate;
+    }
+
+    public void setContractDate(String contractDate) {
+        this.contractDate = contractDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+    public abstract double getTotalPrice();
+    public abstract double getMonthlypayment();
 }
---
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -0,0 +1,79 @@
+package com.pluralsight;
+
+public class SalesContract extends Contract{
+    private double salesTaxAmount;
+    private double recordingFee;
+    private double processingFee;
+    private boolean Financeoption;
+    double Price=0;
+    double monthlyPayment=0;
+
+    public double getSalesTaxAmount() {
+        return salesTaxAmount;
+    }
+
+    public void setSalesTaxAmount(double salesTaxAmount) {
+        this.salesTaxAmount = salesTaxAmount;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public void setRecordingFee(double recordingFee) {
+        this.recordingFee = recordingFee;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinancedoption() {
+        return Financeoption;
+    }
+
+    public void setFinanceoption(boolean financeoption) {
+        Financeoption = financeoption;
+    }
+
+    public SalesContract(String contractDate, String customerName, String customerEmail, Vehicle vehicleSold , double salesTaxAmount, double recordingFee, double processingFee, boolean isFinanced) {
+        super(contractDate, customerName, customerEmail, vehicleSold);
+        this.salesTaxAmount = salesTaxAmount;
+        this.recordingFee = recordingFee;
+        this.processingFee = processingFee;
+        this.Financeoption = isFinanced;
+    }
+
+
+    @Override
+    public double getTotalPrice() {
+        if (Financeoption) {
+            Price =(recordingFee+processingFee+getMonthlypayment())+(recordingFee+processingFee+getMonthlypayment())*(salesTaxAmount/100);
+        } else {
+            Price =(getVehicleSold().getPrice()+recordingFee+processingFee) +(getVehicleSold().getPrice()+recordingFee+processingFee)*(salesTaxAmount/100);
+        }
+
+        return Price;
+
+    }
+
+
+    @Override
+    public double getMonthlypayment() {
+        if (!Financeoption) {
+            return 0; // If the user chose not to take out a loan the monthly payment is 0
+        }
+        // If the cost of the vehicle is > 10,000
+        if (getVehicleSold().getPrice()>10000){
+            monthlyPayment = ((getVehicleSold().getPrice()/48)*(0.00425));
+        } else { // < 10,000
+            monthlyPayment = ((getVehicleSold().getPrice()/24)*(0.00525));
+        }
+        return monthlyPayment;
+
+    }
+}
